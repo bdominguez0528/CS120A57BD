d5a14dfe2af705f31a87d3d014d2bb188fbc26bb
Bryant Dominguez <bdominguez0528@gmail.com>
2024-09-03 11:26:39 -0700

    CS120A57

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n1, n2;
+	int N;
+
+	cin >> n1 >> n2;
+	cin >> N;
+
+	/***************************************************
+	 * Code your program here
+	 **************************************************	*/
+}
